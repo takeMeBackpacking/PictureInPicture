43725be9562a5f436d24adc83e24bed6dc2a5e0f
takeMeBackpacking <jln1589@gmail.com>
2022-09-13 22:26:32 -0700

    second commits

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log("hello");
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+// prompt user to select media stream, pass to video element, play
+async function selectMediaStream() {
+    try {
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        }
+
+    } catch (error) {
+        // catch errors
+        console.log('there was an error ', error);
+    }
+}
+
+button.addEventListener('click', async () => {
+    // button disabled
+    button.disabled = true;
+
+    // start pic in pic
+    await videoElement.requestPictureInPicture();
+
+    // reset button
+    button.disabled = false;
+
+});
+
+selectMediaStream();
